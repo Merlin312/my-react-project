5345ffbca0b9f6190657d25c38dae6b1d2ed92c8
AlexRado <kia8247@gmail.com>
2024-05-15 10:49:12 +0300

    practice, added button doneAll

--- src/projects/todo/components/todoItem/ComplatedButton.jsx
+++ src/projects/todo/components/todoItem/ComplatedButton.jsx
@@ -1,16 +1,35 @@
 import { useState } from 'react';
 
 const ComplatedButton = ({ todo, addTodo }) => {
+  const [done, setDone] = useState(true);
   const markAllAsCompleted = () => {
-    const updatedTodos = todo.map((item) => {
-      return { ...item, completed: true };
+    setDone(done == true ? false : true);
+    const changeComplated = todo.map((item) => {
+      if (done) {
+        return { ...item, completed: true };
+      } else {
+        return { ...item, completed: false };
+      }
     });
-    addTodo(updatedTodos);
+    console.log(done);
+    addTodo(changeComplated);
   };
   return (
     <div>
-      {' '}
-      <button onClick={markAllAsCompleted}>DoneEvrenthing</button>{' '}
+      <button
+        onClick={markAllAsCompleted}
+        style={{
+          color: 'rgba(100, 200, 200, 0.2)',
+          padding: '0.5em 1em',
+          backgroundColor: 'rgba(8, 0, 0, 0.2)',
+          marginTop: '10px',
+          borderRadius: '6px',
+          border: 'none',
+          boxShadow: '4px 4px 10px rgba(0, 0, 0, 0.2)',
+        }}
+      >
+        {done == true ? 'DoneAll' : 'UndoneAll'}
+      </button>
     </div>
   );
 };
